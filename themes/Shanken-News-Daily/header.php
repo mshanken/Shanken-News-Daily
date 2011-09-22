f94c9951b8289a378a6ae4758ffd9fe489719207
Stephen Cannizzaro <cannizzaro75@gmail.com>
2011-09-22 11:43:15 -0400

    fixed news headings and meta viewport code

--- themes/Shanken-News-Daily/header.php
+++ themes/Shanken-News-Daily/header.php
@@ -10,7 +10,7 @@
 	
 	<title><?php wp_title( '|', true, 'right' );?></title>
 	
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 	<meta name="google-site-verification" content="1dujorfLxBn9bxsL2tI-GQ-9A9kwxQj5QaLVAWnP8Ac" />
 	<meta name="google-site-verification" content="gm0xsPL2pF96o9vlfzKV1MDNDMOh6bUixNkNyN_0_zE" />
 	
