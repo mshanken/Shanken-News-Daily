79fe7a2f8cf9563e632f285f5564a80b90fb5092
Stephen Cannizzaro <cannizzaro75@gmail.com>
2011-11-10 16:21:25 -0500

    updted latest jquery version

--- themes/Shanken-News-Daily/functions.php
+++ themes/Shanken-News-Daily/functions.php
@@ -42,7 +42,7 @@
  *	load all scripts */
 if ( !is_admin() ) {
 	wp_deregister_script('jquery');
-	wp_register_script('jquery', ("https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"), false);
+	wp_register_script('jquery', ("https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"), false);
 	wp_enqueue_script('jquery');
 	
 	wp_register_script('modernizr', get_bloginfo('template_directory') . "/js/libs/modernizr-1.7.min.js");
