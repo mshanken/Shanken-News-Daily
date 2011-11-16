65a7473cd0d5289ae84d991cb0c69de650cec807
Stephen Cannizzaro <cannizzaro75@gmail.com>
2011-11-16 10:17:07 -0500

    added impact ads for each view

--- themes/Shanken-News-Daily/sidebar.php
+++ themes/Shanken-News-Daily/sidebar.php
@@ -7,7 +7,19 @@
 									<input id="search-submit" type="submit" value=""/>
 								</div><!--/search-input-->
 							</div><!--/search-wrap-->
-						</form><!--/searchform-->
+		</form><!--/searchform-->
+		<div class="widget-container" id="ad-desktop">
+			<a href="http://www.winespectator.com/micro/show?id=40690" title="The 36th annual Impact Marketing Seminar - March 22, 2012"><img src="<?php bloginfo('template_url'); ?>/img/IMP-MS2011-274x220.jpg" alt="The 36th annual Impact Marketing Seminar - March 22, 2012" /></a></div>
+			
+		<div class="widget-container" id="ad-tablet">
+			<a href="http://www.winespectator.com/micro/show?id=40690" title="The 36th annual Impact Marketing Seminar - March 22, 2012"><img src="<?php bloginfo('template_url'); ?>/img/IMP-MS2011-189x150.jpg" alt="The 36th annual Impact Marketing Seminar - March 22, 2012" /></a></div>
+			
+		<div class="widget-container" id="ad-mobile-landscape">
+			<a href="http://www.winespectator.com/micro/show?id=40690" title="The 36th annual Impact Marketing Seminar - March 22, 2012"><img src="<?php bloginfo('template_url'); ?>/img/IMP-MS2011-360x200.jpg" alt="The 36th annual Impact Marketing Seminar - March 22, 2012" /></a></div>
+			
+		<div class="widget-container" id="ad-mobile-port">
+			<a href="http://www.winespectator.com/micro/show?id=40690" title="The 36th annual Impact Marketing Seminar - March 22, 2012"><img src="<?php bloginfo('template_url'); ?>/img/IMP-MS2011-200x150.jpg" alt="The 36th annual Impact Marketing Seminar - March 22, 2012" /></a></div>
+
 <?php
 	/* When we call the dynamic_sidebar() function, it'll spit out
 	 * the widgets for that widget area. If it instead returns false,
