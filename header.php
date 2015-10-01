7f61a18c3a2b0dcfd4f9674daf48d187ba2dd310
nicksglobal@gmail.com <nicksglobal@gmail.com>
2015-10-01 16:18:11 -0400

    started making the snd site responsive

--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
 <!--[if IE 7]> <html class="no-js ie7 oldie" lang="en"> <![endif]-->
 <!--[if IE 8]> <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -20,11 +21,12 @@
 	<link rel="shortcut icon" href="/wp-content/themes/Shanken-News-Daily/img/favicon.ico">
 	<link rel="apple-touch-icon" href="/wp-content/themes/Shanken-News-Daily/img/apple-touch-icon.png">
 
-
+	<link href="/wp-content/themes/Shanken-News-Daily/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="/wp-content/themes/Shanken-News-Daily/style.css">
+	<!-- <link rel="stylesheet" href="/wp-content/themes/Shanken-News-Daily/mystyle.css"> -->
 	<!-- <link rel="stylesheet" href="http://shankennewsdaily.com/wp-content/plugins/contact-form-7/styles.css"> -->
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600">
-	
+
 	<script src="https://s3.amazonaws.com/toolkit.mshanken.com/js/modernizr.min.js"></script>
 	<?php wp_head(); ?>
 </head>
@@ -34,23 +36,55 @@
 		<div class="page">
 			<div class="content">
 				<header class="clearfix"><!-- start header -->
-					<div class="one_third">
+					<!-- <div class="one_third">
 						<a href="<?php echo home_url( '/' ); ?>" alt="Shanken News Daily" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
 						<img id="logoimg" src="/wp-content/themes/Shanken-News-Daily/img/drinksdaily-logo2.png" alt="Shanken News Daily" title="Shanken News Daily"/></a>
 					</div>
 					<div class="two_third" id="tagline">
 					<h2><?php bloginfo( 'description' ); ?></h2>
-					</div>
-					<nav class="one_full"><!-- nav -->
+					</div> -->
+					<!-- <nav class="one_full"><!-- nav -->
 						<span id="date"><?php echo date('F j, Y'); ?></span>
-							<ul>
+							<!-- <ul>
 								<li<?php if ( is_home()){echo " class=\"current_page_item\"";}?>><a href="/" title="NEWS HOME">NEWS</a></li>
 					<?php wp_list_pages('exclude=579&title_li='); ?>
 								<li><a href="http://pubform.shankennewsdaily.com/" title="M. Shanken Communications Publications">PUBLICATIONS</a></li>
 								<li id="subpage"><a href="http://newsletters.shankennewsdaily.com" title="Subscribe to Shanken News Daily">SUBSCRIBE to SND</a></li>
-							</ul>
+							</ul> -->
 					</nav><!-- end nav -->
 
 				</header><!-- end header -->
+
+				<div class="container"><!-- start container -->
+
+					<div class="row">
+					  <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+						<a href="<?php echo home_url( '/' ); ?>" alt="Shanken News Daily" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+						<img id="logoimg" src="/wp-content/themes/Shanken-News-Daily/img/drinksdaily-logo2.png" alt="Shanken News Daily" title="Shanken News Daily" class="img-responsive" /></a>
+					  </div><!-- col-lg-4 col-md-4 col-sm-1 col-xs-1 -->
+					  <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12" id="tagline" class="img-responsive">
+						<h2><?php bloginfo( 'description' ); ?></h2>
+					  </div><!-- col-md-8 -->
+					</div><!-- row -->
+
+					<nav class="one_full">
+						<div class="row" >
+						<div class="col-lg-12 col-md-8 col-sm-1 col-xs-1"><span id="date"><?php echo date('F j, Y'); ?></span>
+							<ul class="nav nav-tabs">
+							  <li<?php if ( is_home()){echo " class=\"current_page_item\"";}?>><a href="/" title="NEWS HOME">NEWS</a></li>
+					<?php wp_list_pages('exclude=579&title_li='); ?>
+								<!-- <li class="current_page_item"><a href="http://localhost:8888/" title="M. Shanken Communications Publications">NEWS</a></li>
+								<li class="page_item page-item-2"><a href="http://localhost:8888/sample-page/" title="M. Shanken Communications Publications">ABOUT</a></li> -->
+								<li class="page_item page-item-2"><a href="http://localhost:8888/sample-page/" title="Subscribe to Shanken News Daily">HELP</a></li>
+								<li class="page_item page-item-2"><a href="http://localhost:8888/sample-page/" title="M. Shanken Communications Publications">EVENTS</a></li>
+								<li class="page_item page-item-2"><a href="http://localhost:8888/sample-page/" title="Subscribe to Shanken News Daily">IMPACT DATABANK REPORTS</a></li>
+								<li class="page_item page-item-2"><a href="http://pubform.shankennewsdaily.com/" title="M. Shanken Communications Publications">PUBLICATIONS</a></li>
+								<li id="subpage"><a href="http://newsletters.shankennewsdaily.com/" title="Subscribe to Shanken News Daily">SUBSCRIBE to SND</a></li>
+							</ul>
+						</div>
+					</div>
+					</nav>
+
+				</div><!-- container -->
 
 <div class="clearfix"></div>
