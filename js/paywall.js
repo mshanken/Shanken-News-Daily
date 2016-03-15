5d78390e0a207e2407dd2979f09b1ab3ef019205
David VanHook <dvanhook@mshanken.com>
2016-03-15 10:14:41 -0400

    Have point to staging, for testing

--- js/paywall.js
+++ js/paywall.js
@@ -150,7 +150,7 @@ function apiCall(email) {
   if(!email)
     return false;
 
-  var url = 'http://www.winespectator.com/api/cIGetUserLists?email='+email;
+  var url = 'http://staging.winespectator.com/api/cIGetUserLists?email='+email;
 
   $.ajax({
     url: url,
