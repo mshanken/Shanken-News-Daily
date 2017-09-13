cda8ab97683ac9d86c731171075f92d9a58f0ecb
Edison Leon <eleon@mshanken.com>
2017-09-13 14:57:22 -0400

    keep login button

--- js/paywall.js
+++ js/paywall.js
@@ -109,16 +109,16 @@ function performRestrictedBusinessLogic() {
   var subscribeLink = 'http://newsletters.shankennewsdaily.com/',
     modalTitleText = 'Less Than $1 Per Day',
     // modalBodyText = 'This website is available to <i>Shanken News Daily</i> subscribers only. Please log in below.<p>If you don\'t have a subscription, <a href="'+subscribeLink+'" onclick="dataLayer.push({\'event\'\: \'modal_paywall_click\'});" style="color:blue">click here to sign up today.</a>',
-    modalBodyText = 'Receive full access to ShankenNewsDaily.com AND <br class="hidden-xs">get Shanken News Daily newsletter in your inbox, every week<span class="hidden-xs">-</span><br class="hidden-xs">day morning. It\'s industry news<br class="hidden-xs"> you won\'t ready anywhere else.<br><a href="'+subscribeLink+'" class="btn btn-default btn-lg" onclick="dataLayer.push({\'event\'\: \'modal_paywall_click\'});" style="color:#396b99; margin:20px auto;">START MY SUBSCRIPTION</a>' 
+    modalBodyText = 'Receive full access to <strong>ShankenNewsDaily.com <br class="hidden-xs">AND</strong> get <strong>Shanken News Daily newsletter</strong> in your <br class="hidden-xs">inbox, every weekday morning. It\'s industry news <br class="hidden-xs">you won\'t ready anywhere else.<br><a href="'+subscribeLink+'" class="btn btn-default btn-lg" onclick="dataLayer.push({\'event\'\: \'modal_paywall_click\'});" style="color:#396b99; margin:20px auto;">START MY SUBSCRIPTION</a>' 
     modalBodyForm = '<br>Already a subscriber?  Log in.\
     <form class="form-inline">\
       <div id="flashMessageWarning" class="alert alert-danger" style="display: none;"></div>\
       <div id="flashMessageSuccess" class="alert alert-success" style="display: none;"></div>\
       <div class="form-group form-group-lg">\
         <label for="subscriberEmail" class="control-label sr-only">Email Address</label>\
-        <input type="email" class="form-control" id="subscriberEmail" placeholder="Email Address">\
+        <input type="email" class="form-control" id="subscriberEmail" placeholder="Email Address" style="border-radius: 0;">\
       </div>\
-      <button id="modalSubmit" class="btn btn-default btn-lg">Log In</button>\
+      <button id="modalSubmit" class="btn btn-success btn-lg">Log In</button>\
       <br>\
     </form>',
     modalImg = '</div><div><img class="img-responsive" src="http://SHANKDNEW-ElasticL-A0CNGVQLG2YI-1454536625.us-east-1.elb.amazonaws.com/wp-content/uploads/2017/09/SND-Blocker.gif" style="margin-bottom:13px;">',
